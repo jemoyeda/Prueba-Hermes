880cb98f2758d37dc5ea6956791eb995900b13e4
Ing. Hermes Colina <79210037+HermesColina2909@users.noreply.github.com>
2022-02-25 15:55:55 -0400

    primer_modulo_correccion

--- Mi_primer_modulo/models/libros.py
+++ Mi_primer_modulo/models/libros.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 #creando un modelo a partir de una clase
 class Libros(models.Model):
-    _name : 'libros'
+    __name : 'libros'
 
     name = fields.Char(string="Nombre del libro")
     name = fields.Char(string="Categoria")
