15fb0dc15248468f0bd8fbaf1601c5ba1edbdaec
Ing. Hermes Colina <79210037+HermesColina2909@users.noreply.github.com>
2022-03-03 15:08:25 -0400

    modulo_1_10

--- modulo_1/models/libros.py
+++ modulo_1/models/libros.py
@@ -5,6 +5,8 @@ class Libros(models.Model):
     _name = 'libros'
 
     name = fields.Char(string="Nombre del libro")
+    Description = fields.Char(string="Descripcion")
+    Autor = fields.Char(string="Autor")
     Price = fields.Float(string="Precio de venta")
     
     
