8ff848ad64d8fe9ceacd5710a46be0feeb9f35ba
Ing. Hermes Colina <79210037+HermesColina2909@users.noreply.github.com>
2022-03-03 08:57:11 -0400

    modulo

---
+++ modulo_1/models/libros.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+#creando un modelo a partir de una clase
+class Libros(models.Model):
+    _name = 'libros'
+
+    name = fields.Char(string="Nombre del libro")
+    name = fields.Char(string="Categoria")
+    name = fields.Float(string="precio de venta")
+    
+    
