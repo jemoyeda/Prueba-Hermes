4d29377c607f7a59e994dc55511e99b1b5ec0219
Ing. Hermes Colina <79210037+HermesColina2909@users.noreply.github.com>
2022-03-04 10:51:03 -0400

    colores_2

--- Color/models/colores.py
+++ Color/models/colores.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
 #creando un modelo a partir de una clase
-class Libros(models.Model):
+class colores(models.Model):
     _name = 'colores'
 
     color = fields.Char(string="Color")
