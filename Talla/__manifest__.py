da2469333739fe98241498b2bffa5ed348d6fb0e
Ing. Hermes Colina <79210037+HermesColina2909@users.noreply.github.com>
2022-03-04 13:22:39 -0400

    tallas_1

---
+++ Talla/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding utf-8 -*-
+
+{
+
+    'name': "Talla",
+    'description': "Modulo para registrar talla de productos",
+    'summary': "Modulo para registrar talla de atributos de productos",
+    "images": ["images/icon.jpg"]
+    'author': 'Ing. Hermes Colina',
+    'Category': 'General',
+    'Version': '1.0.0.0',
+    'depends':[],
+    'data':[
+
+            'views/menu_view.xml',
+            'security/tallas_security.xml',
+            'security/ir.model.access.csv',
+        ],
+    
+
+}
